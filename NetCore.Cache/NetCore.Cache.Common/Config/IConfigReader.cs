6b39623ba169422d386775b63b3b9376f02d0eb8
Martin Zhang <bamboo.zhang@daimler.com>
2019-03-18 12:54:17 +0800

    Initialize project.

---
+++ NetCore.Cache/NetCore.Cache.Common/Config/IConfigReader.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCore.Cache.Common.Config
+{
+    public interface IConfigReader
+    {
+        int? GetIntNullable(string appKey);
+        bool GetBool(string key);
+        string GetString(string key);
+    }
+}
